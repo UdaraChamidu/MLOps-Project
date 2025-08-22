ec479ce8be22469d6b610511a639ac3ad66df218
udara <chamiduudara321@gmail.com>
2025-08-22 23:26:07 +0530

    dockerfile added

--- test_app.py
+++ test_app.py
@@ -1,1 +1,10 @@
 # unit testing things are added here. it will automatically detect.(because start with test_)
+
+from app import app
+
+def test_home():           # in this test_app.py, should starts methods with test_ (will detect automatically)
+    response = app.test_client().get('/')  # geta from the home page
+    
+    assert response.status_code == 200    # check major response (successful request)
+    assert response.data == b"Hello, World!"
+
